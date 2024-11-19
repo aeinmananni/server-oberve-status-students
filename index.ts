9bd89ef924a6c152948a04718cdb80e5c27f51ce
aein <mananniaein1234@gmail.com>
2024-11-19 11:30:12 +0330

    qw

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Application }  from "express";
 import "dotenv-safe/config";
 const app = express();
 import ("./router/router").then((module) =>{
-    (module as npm as {default:(app:Application) => void}).default(app);
+    (module as any as {default:(app:Application) => void}).default(app);
 })
 const port = process.env.PORT || 3030;
 app.listen(port ,()=> console.log(`Listening On Port : ${port}`));
